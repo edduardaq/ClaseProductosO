e5fe584244e24ff9c9a1dab4593dc76aeee232c1
edduardaq <Andresquiros.edgmail.com>
2023-07-29 09:58:01 -0600

    Ultimo push

---
+++ src/app/shared/formsModels/productosForms.ts
@@ -0,0 +1,11 @@
+import { FormBuilder, FormGroupName } from "@angular/forms";
+
+export class ProductosForm{
+
+    baseForm: FormGroupName;
+    constructor(private fb: FormBuilder){
+         
+    }
+
+
+}
